38afee46d07655c45a98aaf77d7b6cd6b79677e3
pixiekat <pixiekat@noreply.codeberg.org>
2026-03-19 22:33:40 -0400

    add bash_logout

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -32,6 +32,7 @@ BACKUP_DIR="$HOME/.dotfiles_backup/$(date +%Y%m%d_%H%M%S)"
 # Add or remove entries as your repo grows
 DOTFILES=(
     .aliases
+    .bash_logout
     .bashrc
     .cache/oh-my-posh/themes/iranian-solidarity.omp.json
     .config/btop/btop.conf
